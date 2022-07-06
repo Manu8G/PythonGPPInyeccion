76a8691c62cca49cfb72fa00d83ed83a425c336d
Manuel Guerrero <manuel8guerrero@hotmail.com>
2022-07-06 14:09:44 +0200

    Creada conexion y comprobada la BD

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import mysql.connector
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    try:
+        conexion = mysql.connector.connect(
+            host='localhost',
+            user='gpp',
+            passwd='1234Hola',
+        )
+        # database='PruebasGPP'
+        print("Conexion exitosa")
+
+        mycursor = conexion.cursor()
+        mycursor.execute("SHOW DATABASES")
+        result = mycursor.fetchall()
+        exist = False
+
+        for i in result:
+            if str(i) == '(\'PruebasGPP\',)':
+                exist = True
+
+        if exist:
+            pass
+        else:
+            mycursor.execute("CREATE DATABASE PruebasGPP")
+
+        mycursor = conexion.cursor()
+        mycursor.execute("SHOW DATABASES")
+        result = mycursor.fetchall()
+        for i in result:
+            print(i)
+
+    except Exception as e:
+        print("Tenemos el siguiente error:\n")
+        print(e)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
